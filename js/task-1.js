f856aebcd4b3d03ba040dee32ee2084d02f91d01
MerezkaYurii <shkolasad@ukr.net>
2024-10-12 16:08:49 +0300

    var1

--- js/task-1.js
+++ js/task-1.js
@@ -1,22 +1,38 @@
-function slugify(title) {
-       const slug = title.toLowerCase() .replaceAll(' ', '-');
-    return slug
+ function isEnoughCapacity(products, containerSize){
+ let total = 0;
+ for (const key in products) {
+ total+= products[key]
+ }
+ if(total<=containerSize){return true}
+ else {return false}
+ }
     
-    }
-    
-    
-    
-    
-    slugify("Arrays for beginners")
-    slugify("English for developer")
-    slugify("Ten secrets of JavaScript") 
-    slugify("How to become a JUNIOR developer in TWO WEEKS")
-    
-    
-    
-    
-    console.log(slugify("Arrays for beginners")); // "arrays-for-beginners"
-    console.log(slugify("English for developer")); // "english-for-developer"
-    console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
-    console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); // "how-to-become-a-junior-developer-in-two-weeks"
-    
+    console.log(isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)); // true
+    console.log(isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)); // false
+    console.log(isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)); // true
+    console.log(isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)); // false
+
+
+
+
+
+
+
+
+
+
+    // function isEnoughCapacity(products, containerSize){
+//    let total = 0;
+//    const product = Object.keys(products);
+// for (const key of product){
+//      total += products[key];
+// }  
+// if (total <= containerSize){return true}
+//   else {return false}
+//        }
+  
+  
+//   console.log(isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)); // true
+//   console.log(isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)); // false
+//   console.log(isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)); // true
+//   console.log(isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)); // false
